9cdc8dfd1f24d31e227929f5cf52d51d3c7e063b
songx827 <songx827@morris.umn.edu>
2018-09-20 16:09:15 -0500

    We clean the changeState() method by moving its logic to setActiveJob().

--- src/applications/Machine.java
+++ src/applications/Machine.java
@@ -9,6 +9,7 @@ class Machine {
     private int totalWait; // total delay at this machine
     private int numTasks; // number of tasks processed on this machine
     private Job activeJob; // job currently active on this machine
+    private int machineNum; // to-do: make machines know their number
 
     // constructor
     Machine() {
@@ -23,12 +24,46 @@ class Machine {
         return jobQ.isEmpty();
     }
 
-    public int setActiveJob(int timeNow){
-        activeJob = (Job) jobQ.remove();
-        totalWait += timeNow - activeJob.getArrivalTime();
-        numTasks++;
-        int finishTaskTime = activeJob.removeNextTask();
-        return finishTaskTime + timeNow;
+    /*
+    If activeJob==null: sets activeJob to the next job on the Queue, unless the jobQ is empty then we change the "state"
+                        of the machine to be idling
+    Else: set the active job to be null, signifying that the machine is in its "change over state"
+
+    Also updates all relevant numbers/fields
+     */
+    public int setActiveJob(int timeNow,EventList eList, int machineNum){
+
+        //If the current active job is null, we try to give it one
+        if(activeJob == null) {
+            activeJob = (Job) jobQ.remove();
+
+            //If the active job is still null, there are no jobs waiting for this machine and we tell eList that it is
+            //idle by setting the finish time to MAX_VALUE
+            if (activeJob == null){
+                eList.setFinishTime(machineNum, Integer.MAX_VALUE);
+                return Integer.MAX_VALUE;
+            } else {
+                totalWait += timeNow - activeJob.getArrivalTime();
+                numTasks++;
+                int finishTaskTime = activeJob.removeNextTask();
+                eList.setFinishTime(machineNum, finishTaskTime + timeNow);
+                return finishTaskTime + timeNow;
+            }
+        }
+        //If the active job is not null then we are finished with the task and we make the active job null
+        else {
+            activeJob = null;
+            eList.setFinishTime(machineNum, changeTime + timeNow);
+            return changeTime + timeNow;
+        }
+    }
+
+    public boolean isIdle(){
+        return jobQ.isEmpty() && activeJob==null;
+    }
+
+    public void setActiveJob(Job activeJob) {
+        this.activeJob = activeJob;
     }
 
     public int getChangeTime() {
@@ -59,7 +94,5 @@ class Machine {
         return activeJob;
     }
 
-    public void setActiveJob(Job activeJob) {
-        this.activeJob = activeJob;
-    }
+
 }
